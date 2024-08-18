8180fdccb6167ffe7175c96b1cdf8a45556a6e43
Theo Morris <pandazflipperpanda706@gmail.com>
2024-08-19 02:12:39 +1200

    stopped the weird mouse delta rotation when pausing

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -11,6 +11,7 @@ public class InputManager : MonoBehaviour
     [HideInInspector] public Vector2 currentMouseDelta = Vector2.zero;
     Vector2 currentMouseDeltaVelocity = Vector2.zero;
     [HideInInspector] public Vector2 MovementVector;
+    private bool updateMouseDelta = true;
 
     private void Awake()
     {
@@ -34,6 +35,7 @@ public class InputManager : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (!updateMouseDelta) return;
 
         Vector2 targetMouseDelta = playerInput.InGame.Look.ReadValue<Vector2>();
 
@@ -53,11 +55,14 @@ public class InputManager : MonoBehaviour
 
     public void DisableInGameInput()
     {
+        updateMouseDelta = false;
+        currentMouseDelta = Vector2.zero;
         playerInput.InGame.Disable();
     }
 
     public void EnableInGameInput()
     {
+        updateMouseDelta = true;
         playerInput.InGame.Enable();
     }
 }
